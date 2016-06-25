1c966ad9ca8cea6e548f76d3251dd146f8cfc07c
yyngfive <chenhye5@outlook.com>
2016-06-25 14:08:40 +0800

    增加内容

--- main.c
+++ main.c
@@ -1,25 +1,33 @@
 #include "main.h"
 
+int main(int argc, char const *argv[])
+{
+    int length;
+    FILE *fp;
+    fp = fopen("test1.x","r");
+    length = get_length(fp);
+    if(length == -1)
+    {
+        printf("%s\n","无法得到长度" );
+    }
+    fclose(fp);
+    printf("%d\n",length);
+    return 0;
+}
+
 int get_length(FILE *file)
 {
     int i,length;
     i = 0;
     length = 0;
+    if (file == NULL)
+    {
+        return -1;
+    }
     for ( i ; feof(file) == 0 ; i++)
     {
             fgetc(file);
     }
     length = i;
     return length;
-}
-
-int main(int argc, char const *argv[])
-{
-    int length;
-    FILE *fp;
-    fp = fopen("test.txt","r");
-    length = get_length(fp);
-    fclose(fp);
-    printf("%d\n",length);
-    return 0;
 }
--- main.h
+++ main.h
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
+/*
+*用于得到文件长度
+*参数：一个文件指针
+*返回：文件长度
+*/
+int get_length(FILE *file);
