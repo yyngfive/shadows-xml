816da494d786f7a5f1eb375fb09b9e3aee75701c
yyngfive <chenhye5@outlook.com>
2016-06-25 15:00:47 +0800

    添加函数

--- main.c
+++ main.c
@@ -2,32 +2,48 @@
 
 int main(int argc, char const *argv[])
 {
-    int length;
+    char data[1024];
     FILE *fp;
     fp = fopen("test1.x","r");
-    length = get_length(fp);
-    if(length == -1)
+    if (file != NULL)
     {
-        printf("%s\n","无法得到长度" );
+        data = format_xml(file);
+        printf("%s\n",data);
+        fclose(fp);
     }
-    fclose(fp);
-    printf("%d\n",length);
+    printf("%s\n","Can not open the file!\n" );
     return 0;
 }
 
 int get_length(FILE *file)
 {
-    int i,length;
+    int i;
     i = 0;
-    length = 0;
-    if (file == NULL)
-    {
-        return -1;
-    }
     for ( i ; feof(file) == 0 ; i++)
     {
             fgetc(file);
     }
-    length = i;
-    return length;
+    return i;
+}
+
+char *format_xml(FILE *xml)
+{
+    char c;
+    const char space = ' ';
+    const char enter1 = '\r';
+    const char enter2 = '\n';
+    const char l_chevron = '<';
+    const char r_chevron = '>';
+
+    int label_leng = 0,file_leng = 0;
+    int i = 0;
+    char *formated = NULL;
+
+    file_leng = get_length(file);
+    formated = (char *)malloc(file_leng+1);
+    while (feof(file) != 0 )
+    {
+        formated[i] = fgetc(file);
+    }
+    return formated;
 }
--- main.h
+++ main.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 /*
 *用于得到文件长度
 *参数：一个文件指针
 *返回：文件长度
 */
 int get_length(FILE *file);
+char *format_xml(FILE *xml);
